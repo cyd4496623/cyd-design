ace898c07d4250d56d80b0a9b4bfeefff383a520
chenyunda <chenyunda@crazymaplestudio.com>
2021-07-26 21:58:17 +0800

    feat: config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Tag from './components/tag'
+
+function App() {
+  return (
+    <div className="App">
+      <Tag />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tag/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const Tag = () => {
+
+  return <div>12312Tag</div>
+}
+
+export default Tag
